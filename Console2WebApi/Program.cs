68dca9ebcb6a796aca536a6c57092b3abde977c0
darapumahesh99 <darapumahesh79@gmail.com>
2022-10-04 12:46:34 +0530

    added

---
+++ Console2WebApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Console2WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IHostBuilder
+    }
+}
